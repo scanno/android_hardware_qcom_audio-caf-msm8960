f9ec7d2bfb6ac4e3cde7689d18da7181cdbc8b34
Kuirong Wang <kuirongw@codeaurora.org>
2014-12-09 22:05:01 -0800

    hal: Update audio HAL to enable the hw plugin APIs
    Update the audio HAL to enable the hw plugin APIs so
    that the external codec can be properly configured.
    
    Change-Id: Iceb19cbadd2f6cb443b8686643da668b1378e87e

--- hal/audio_extn/extn_adsp_plugin.c
+++ hal/audio_extn/extn_adsp_plugin.c
@@ -269,56 +269,72 @@ int audio_extn_ext_hw_plugin_set_parameters(void *plugin, struct str_parms *parm
         ALOGD("%s: received plugin msg type (%d)", __func__, val);
         str_parms_del(parms, AUDIO_PARAMETER_KEY_EXT_HW_PLUGIN_MSG_TYPE);
         if (my_plugin->audio_hal_plugin_send_msg) {
-            switch(val) {
-            case AUDIO_HAL_PLUGIN_MSG_CODEC_SET_PP_VOLUME:
-            {
-                audio_hal_plugin_codec_set_pp_vol_t pp_vol;
+            if (val == AUDIO_HAL_PLUGIN_MSG_CODEC_TUNNEL_CMD) {
+                /* TODO - handle tunnel cmd here */
+            } else {
+                audio_usecase_t use_case;
+                snd_device_t snd_dev;
+
                 err = str_parms_get_int(parms, AUDIO_PARAMETER_KEY_EXT_HW_PLUGIN_UC,
-                    &pp_vol.usecase);
+                    &use_case);
                 if (err >= 0) {
-                    str_parms_del(parms, AUDIO_PARAMETER_KEY_EXT_HW_PLUGIN_UC);
+                   str_parms_del(parms, AUDIO_PARAMETER_KEY_EXT_HW_PLUGIN_UC);
                 } else {
-                    /* TODO: properly handle no uc param from client case */
+                    /* TODO: do we need to support no use case in kvpair? */
                     return -EINVAL;
                 }
-                err = str_parms_get_int(parms, AUDIO_PARAMETER_KEY_EXT_HW_PLUGIN_CMASK,
-                   (int*)&pp_vol.ch_mask);
-                if ((err >= 0)) {
-                    str_parms_del(parms, AUDIO_PARAMETER_KEY_EXT_HW_PLUGIN_CMASK);
-                } else {
-                    /* TODO: properly handle no cmask param from client case */
-                    return -EINVAL;
-                }
-                err = str_parms_get_int(parms, AUDIO_PARAMETER_KEY_EXT_HW_PLUGIN_GAIN,
-                    (int*)&pp_vol.gain);
-                if ((err >= 0)) {
-                    str_parms_del(parms, AUDIO_PARAMETER_KEY_EXT_HW_PLUGIN_GAIN);
-                } else {
-                    /* TODO: properly handle no gain param from client case */
-                    return -EINVAL;
-                }
-                if ((pp_vol.usecase>= 0) && (pp_vol.usecase < AUDIO_USECASE_MAX)
-                    && (pp_vol.ch_mask)) {
+
+                if ((use_case>= 0) && (use_case < AUDIO_USECASE_MAX)) {
                     struct audio_usecase *uc_info;
-                    audio_hal_plugin_msg_type_t msg =
-                        AUDIO_HAL_PLUGIN_MSG_CODEC_SET_PP_VOLUME;
-                    uc_info = get_usecase_from_list(my_plugin->adev, pp_vol.usecase);
+                    uc_info = get_usecase_from_list(my_plugin->adev, use_case);
                     if (uc_info == NULL) {
                         ALOGE("%s: Could not find the usecase (%d) in the list",
-                            __func__, pp_vol.usecase);
-                        /* TODO: cache the setting and apply when the usecase is active */
+                            __func__, use_case);
+                        /* TBD: do we need to cache the setting when usecase is not active? */
                         return -EINVAL;
                     }
                     /* TODO: confirm this handles all usecase */
                     if (uc_info->out_snd_device) {
-                        pp_vol.snd_dev = uc_info->out_snd_device;
-                    } else if(uc_info->in_snd_device) {
-                        pp_vol.snd_dev = uc_info->in_snd_device;
+                        snd_dev = uc_info->out_snd_device;
+                    } else if (uc_info->in_snd_device) {
+                        snd_dev = uc_info->in_snd_device;
                     } else {
                         ALOGE("%s: No valid snd_device found for the usecase (%d)",
-                            __func__, pp_vol.usecase);
+                            __func__, use_case);
                         return -EINVAL;
                     }
+                }else {
+                    return -EINVAL;
+                }
+
+                switch(val) {
+                case AUDIO_HAL_PLUGIN_MSG_CODEC_SET_PP_VOLUME:
+                {
+                    audio_hal_plugin_codec_set_pp_vol_t pp_vol;
+                    memset(&pp_vol,0,sizeof(pp_vol));
+                    err = str_parms_get_int(parms, AUDIO_PARAMETER_KEY_EXT_HW_PLUGIN_CMASK,
+                       (int*)&pp_vol.ch_mask);
+                    if ((err >= 0)) {
+                        str_parms_del(parms, AUDIO_PARAMETER_KEY_EXT_HW_PLUGIN_CMASK);
+                    } else {
+                        /* TODO: properly handle no cmask param from client case */
+                        ALOGE("%s: Invalid or missing CMASK param for SET_PP_VOLUME", __func__);
+                        return -EINVAL;
+                    }
+                    err = str_parms_get_int(parms, AUDIO_PARAMETER_KEY_EXT_HW_PLUGIN_GAIN,
+                        (int*)&pp_vol.gain);
+                    if ((err >= 0)) {
+                        str_parms_del(parms, AUDIO_PARAMETER_KEY_EXT_HW_PLUGIN_GAIN);
+                    } else {
+                        /* TODO: properly handle no gain param from client case */
+                        return -EINVAL;
+                    }
+
+                    audio_hal_plugin_msg_type_t msg =
+                        AUDIO_HAL_PLUGIN_MSG_CODEC_SET_PP_VOLUME;
+
+                    pp_vol.usecase = use_case;
+                    pp_vol.snd_dev= snd_dev;
 
                     ALOGD("%s: sending codec pp vol msg to HAL plugin driver, %d, %d, %d, %d",
                         __func__, (int)pp_vol.usecase, (int)pp_vol.snd_dev,
@@ -329,30 +345,176 @@ int audio_extn_ext_hw_plugin_set_parameters(void *plugin, struct str_parms *parm
                     if (ret) {
                         ALOGE("%s: Failed to set plugin pp vol err: %d", __func__, ret);
                     }
+                    break;
                 }
-                break;
-            }
-            case AUDIO_HAL_PLUGIN_MSG_CODEC_SET_PP_MUTE:
-                /* TODO: */
-                break;
-            case AUDIO_HAL_PLUGIN_MSG_CODEC_SET_PP_FADE:
-                /* TODO: */
-                break;
-            case AUDIO_HAL_PLUGIN_MSG_CODEC_SET_PP_BALANCE:
-                /* TODO:  */
-                break;
-            case AUDIO_HAL_PLUGIN_MSG_CODEC_SET_PP_BMT:
-                /* TODO:  */
-                break;
-            case AUDIO_HAL_PLUGIN_MSG_CODEC_SET_PP_EQ:
-                /* TODO:  */
-                break;
-            case AUDIO_HAL_PLUGIN_MSG_CODEC_TUNNEL_CMD:
-                /* TODO:  */
-                break;
-            default:
-                ALOGE("%s: Invalid plugin message type: %d", __func__, val);
-                return -EINVAL;
+                case AUDIO_HAL_PLUGIN_MSG_CODEC_SET_PP_MUTE:
+                {
+                    int32_t flag;
+                    audio_hal_plugin_codec_set_pp_mute_t pp_mute;
+                    memset(&pp_mute,0,sizeof(pp_mute));
+                    err = str_parms_get_int(parms, AUDIO_PARAMETER_KEY_EXT_HW_PLUGIN_CMASK,
+                       (int*)&pp_mute.ch_mask);
+                    if ((err >= 0)) {
+                        str_parms_del(parms, AUDIO_PARAMETER_KEY_EXT_HW_PLUGIN_CMASK);
+                    } else {
+                        /* TODO: properly handle no cmask param from client case */
+                        ALOGE("%s: Invalid or missing CMASK param for SET_PP_MUTE", __func__);
+                        return -EINVAL;
+                    }
+                    err = str_parms_get_int(parms, AUDIO_PARAMETER_KEY_EXT_HW_PLUGIN_MUTE_FLAG,
+                        (int*)&flag);
+                    if ((err >= 0)) {
+                        str_parms_del(parms, AUDIO_PARAMETER_KEY_EXT_HW_PLUGIN_MUTE_FLAG);
+                        pp_mute.flag = (bool)flag;
+                    } else {
+                        ALOGE("%s: Invalid or missing FLAG param for SET_PP_MUTE", __func__);
+                        return -EINVAL;
+                    }
+
+
+                    audio_hal_plugin_msg_type_t msg =
+                        AUDIO_HAL_PLUGIN_MSG_CODEC_SET_PP_MUTE;
+
+                    pp_mute.usecase = use_case;
+                    pp_mute.snd_dev= snd_dev;
+
+                    ALOGD("%s: sending codec pp mute msg to HAL plugin driver, %d, %d, %d, %d",
+                        __func__, (int)pp_mute.usecase, (int)pp_mute.snd_dev,
+                        (int)pp_mute.ch_mask, (int)pp_mute.flag);
+
+                    ret = my_plugin->audio_hal_plugin_send_msg(msg, &pp_mute,
+                        sizeof(pp_mute));
+                    if (ret) {
+                        ALOGE("%s: Failed to set plugin pp vol err: %d", __func__, ret);
+                    }
+                    break;
+                }
+                case AUDIO_HAL_PLUGIN_MSG_CODEC_SET_PP_FADE:
+                {
+                    audio_hal_plugin_codec_set_pp_fade_t pp_fade;
+                    memset(&pp_fade,0,sizeof(pp_fade));
+                    err = str_parms_get_int(parms, AUDIO_PARAMETER_KEY_EXT_HW_PLUGIN_FADE,
+                        (int*)&pp_fade.fade);
+                    if ((err >= 0)) {
+                        str_parms_del(parms, AUDIO_PARAMETER_KEY_EXT_HW_PLUGIN_FADE);
+                    } else {
+                        ALOGE("%s: Invalid or missing FADE param for SET_PP_FADE", __func__);
+                        return -EINVAL;
+                    }
+
+                    audio_hal_plugin_msg_type_t msg =
+                            AUDIO_HAL_PLUGIN_MSG_CODEC_SET_PP_FADE;
+
+                    pp_fade.usecase = use_case;
+                    pp_fade.snd_dev= snd_dev;
+
+                    ALOGD("%s: sending codec pp fade msg to HAL plugin driver, %d, %d, %d",
+                        __func__, (int)pp_fade.usecase, (int)pp_fade.snd_dev,
+                        (int)pp_fade.fade);
+
+                    ret = my_plugin->audio_hal_plugin_send_msg(msg, &pp_fade,
+                            sizeof(pp_fade));
+                    if (ret) {
+                        ALOGE("%s: Failed to set plugin pp fade err: %d", __func__, ret);
+                    }
+                    break;
+                }
+                case AUDIO_HAL_PLUGIN_MSG_CODEC_SET_PP_BALANCE:
+                {
+                    audio_hal_plugin_codec_set_pp_balance_t pp_balance;
+                    memset(&pp_balance,0,sizeof(pp_balance));
+                    err = str_parms_get_int(parms, AUDIO_PARAMETER_KEY_EXT_HW_PLUGIN_BALANCE,
+                        (int*)&pp_balance.balance);
+                    if ((err >= 0)) {
+                        str_parms_del(parms, AUDIO_PARAMETER_KEY_EXT_HW_PLUGIN_BALANCE);
+                    } else {
+                        ALOGE("%s: Invalid or missing balance param for SET_PP_BALANCE", __func__);
+                        return -EINVAL;
+                    }
+
+                    audio_hal_plugin_msg_type_t msg =
+                            AUDIO_HAL_PLUGIN_MSG_CODEC_SET_PP_BALANCE;
+
+                    pp_balance.usecase = use_case;
+                    pp_balance.snd_dev= snd_dev;
+
+                    ALOGD("%s: sending codec pp balance msg to HAL plugin driver, %d, %d, %d",
+                        __func__, (int)pp_balance.usecase, (int)pp_balance.snd_dev,
+                        (int)pp_balance.balance);
+
+                    ret = my_plugin->audio_hal_plugin_send_msg(msg, &pp_balance,
+                            sizeof(pp_balance));
+                    if (ret) {
+                        ALOGE("%s: Failed to set plugin pp balance err: %d", __func__, ret);
+                    }
+                    break;
+                }
+                case AUDIO_HAL_PLUGIN_MSG_CODEC_SET_PP_BMT:
+                {
+                    int32_t filter_type, enable_flag;
+                    audio_hal_plugin_codec_set_pp_bmt_t pp_bmt;
+                    memset(&pp_bmt,0,sizeof(pp_bmt));
+                    err = str_parms_get_int(parms, AUDIO_PARAMETER_KEY_EXT_HW_PLUGIN_BMT_FTYPE,
+                        (int*)&filter_type);
+                    if ((err >= 0)) {
+                        str_parms_del(parms, AUDIO_PARAMETER_KEY_EXT_HW_PLUGIN_BMT_FTYPE);
+                        if ((filter_type > AUDIO_HAL_PLUGIN_CODEC_PP_FILTER_TYPE_INVALID) &&
+                            (filter_type < AUDIO_HAL_PLUGIN_CODEC_PP_FILTER_TYPE_MAX))
+                            pp_bmt.filter_type = filter_type;
+                        else {
+                            ALOGE("%s: Invalid filter type value for SET_PP_BMT", __func__);
+                            return -EINVAL;
+                        }
+                    } else {
+                        ALOGE("%s: Invalid or missing filter type param for SET_PP_BMT", __func__);
+                        return -EINVAL;
+                    }
+                    err = str_parms_get_int(parms, AUDIO_PARAMETER_KEY_EXT_HW_PLUGIN_BMT_FLAG,
+                        (int*)&enable_flag);
+                    if ((err >= 0)) {
+                        str_parms_del(parms, AUDIO_PARAMETER_KEY_EXT_HW_PLUGIN_BMT_FLAG);
+                        pp_bmt.enable_flag = (bool)enable_flag;
+                    } else {
+                        ALOGE("%s: Invalid or missing enable flag param for SET_PP_BMT", __func__);
+                        return -EINVAL;
+                    }
+                    err = str_parms_get_int(parms, AUDIO_PARAMETER_KEY_EXT_HW_PLUGIN_BMT_VAL,
+                        (int*)&pp_bmt.value);
+                    if ((err >= 0)) {
+                        str_parms_del(parms, AUDIO_PARAMETER_KEY_EXT_HW_PLUGIN_BMT_VAL);
+                    } else {
+                        ALOGE("%s: Invalid or missing value param for SET_PP_BMT", __func__);
+                        return -EINVAL;
+                    }
+
+                    audio_hal_plugin_msg_type_t msg =
+                            AUDIO_HAL_PLUGIN_MSG_CODEC_SET_PP_BMT;
+
+                    pp_bmt.usecase = use_case;
+                    pp_bmt.snd_dev= snd_dev;
+
+                    ALOGD("%s: sending codec pp bmt msg to HAL plugin driver,%d,%d,%d,%d,%d",
+                        __func__, (int)pp_bmt.usecase, (int)pp_bmt.snd_dev,
+                        (int)pp_bmt.filter_type, (int)pp_bmt.enable_flag,
+                        (int)pp_bmt.value);
+
+                    ret = my_plugin->audio_hal_plugin_send_msg(msg, &pp_bmt,
+                            sizeof(pp_bmt));
+                    if (ret) {
+                        ALOGE("%s: Failed to set plugin pp bmt err: %d", __func__, ret);
+                    }
+                    break;
+                }
+                case AUDIO_HAL_PLUGIN_MSG_CODEC_SET_PP_EQ:
+                    /* TODO:  */
+                    break;
+                case AUDIO_HAL_PLUGIN_MSG_CODEC_TUNNEL_CMD:
+                    /* TODO:  */
+                    break;
+                default:
+                    ALOGE("%s: Invalid plugin message type: %d", __func__, val);
+                    return -EINVAL;
+                }
             }
         }
     }
